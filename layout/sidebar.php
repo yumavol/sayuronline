49cf00a48eb9d9f349a85b3fc427ce4f1ae6b76f
irsyad fauzan <irsyad.fauzan777@gmail.com>
2018-01-27 13:29:03 +0700

    create database

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,14 +1,18 @@
-          
-
-
     <div class="col-md-3">
       <div style="min-height: 500px; padding-left: 10px;" class="box box-solid">
         <div class="box-body">
           <ul class="nav nav-pills nav-stacked">
+            <?php
+            if(get_session('user_login')) {
+            ?>
             <li role="presentation"><a href="#"><i class="fa fa-archive"></i> Produk</a></li>
             <li role="presentation" class="active"><a href="#"><i class="fa fa-shopping-cart"></i>Keranjang</a></li>
             <li role="presentation"><a href="#"><i class="fa fa-credit-card"></i>Pemesanan</a></li>
             <li role="presentation"><a href="#"><i class="fa fa-sign-out"></i>Logout</a></li>
+            <?php } else { ?>
+            <li role="presentation"><a href="#"><i class="fa fa-sign-in"></i> Login</a></li>
+            <li role="presentation"><a href="#"><i class="fa fa-users"></i>Daftar</a></li>
+            <?php } ?>
           </ul>
         </div>
       </div><!-- /.box -->
